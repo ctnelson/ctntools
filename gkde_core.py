1787473ecfae052029e3d49a64c3faa58476368f
ctnelson <ctnelson@umich.edu>
2023-03-27 12:23:27 -0400

    Add files via upload

---
+++ gkde_core.py
@@ -0,0 +1,70 @@
+import numpy as np
+from scipy import signal
+
+#Kernel Density Estimation
+def gkde_core(ix,iy,ival,s,ixx,iyy,interplinear=True):
+    #Inputs:
+    #ix & iy        :   input xy positions
+    #ival           :   value at the datapoint
+    #s              :   sigma for gausian kernel
+    #ixx & iyy      :   meshgrid of x and y positions to interpolate onto
+    #interplinear   :   whether to calculate from subpixel positions or linear interpolate from pixel positions (former will be slower)
+
+
+    if interplinear:
+        def gkernel(s,wincutoff=3):
+            if np.size(s)==1:
+                s = [s,s]
+            xv = np.arange(-np.ceil(s[0])*wincutoff,np.ceil(s[0])*wincutoff+1)
+            yv = np.arange(-np.ceil(s[1])*wincutoff,np.ceil(s[1])*wincutoff+1)
+            xx,yy = np.meshgrid(xv, yv)
+            z =  1/(2*np.pi*s[0]*s[1]) * np.exp(-.5*xx**2/s[0]**2 - .5*yy**2/s[1]**2)
+            return z
+
+        #get guassian kernel
+        k = gkernel(s)
+
+        xv = ixx[0,:]
+        yv = iyy[:,0]
+
+        #estimate each datapoint as sum of 4 at integer positions
+        stpsz = xv[1]-xv[0]
+        xpos = np.floor((ix-xv[0])/stpsz+1).astype('int')     #get first position
+        xh = (ix-(xv[xpos-1]))/stpsz                          #fraction of xpos
+        xl = 1-xh                                             #fraction of xpos+1
+        stpsz = yv[1]-yv[0]
+        ypos = np.floor((iy-yv[0])/stpsz+1).astype('int')
+        yh = (iy-(yv[ypos-1]))/stpsz
+        yl = 1-yh
+
+        #values
+        vals[ypos-1,xpos-1] = vals[ypos-1,xpos-1]   + xl*yl*ival    #p00
+        vals[ypos,xpos-1]   = vals[ypos,xpos-1]     + xl*yh*ival    #p10
+        vals[ypos-1,xpos]   = vals[ypos-1,xpos]     + xh*yl*ival    #p01
+        vals[ypos,xpos]     = vals[ypos,xpos]       + xh*yh*ival    #p11
+
+        #density
+        dens = np.zeros_like(ixx).astype('float')
+        dens[ypos-1,xpos-1] = dens[ypos-1,xpos-1]   + xl*yl    #p00
+        dens[ypos,xpos-1]   = dens[ypos,xpos-1]     + xl*yh    #p10
+        dens[ypos-1,xpos]   = dens[ypos-1,xpos]     + xh*yl    #p01
+        dens[ypos,xpos]     = dens[ypos,xpos]       + xh*yh    #p11
+
+        #convolve w/ kernel
+        densitymap = signal.convolve2d(dens,k,mode='same')
+        valsmap = signal.convolve2d(vals,k,mode='same')
+        valsmap = np.divide(valsmap,densitymap)
+
+    else:
+        densitymap = np.zeros_like(ixx).astype('float')
+        valsmap = np.zeros_like(ixx).astype('float')
+        for xx in ixx:
+            for yy in iyy:
+                for i in np.arange(ix.size):
+                    dx = ixx[yy,xx] - ix[i]
+                    dy = iyy[yy,xx] - iy[i]
+                    z = 1/(2*np.pi*s[0]*s[1]) * np.exp(-.5*dx**2/s[0]**2 - .5*dy**2/s[1]**2)
+                    densitymap[yy,xx] = densitymap[yy,xx] + z
+                    valsmap[yy,xx] = valsmap[yy,xx]+z*ival[i]
+
+    return densitymap, valsmap
---
+++ imregionalmax.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+#finds regional maximum values (pixel-level)
+def imregionalmax(inim, windowsize, insubmask = np.empty([0],dtype='bool'), prominence=0, normalmode=0, exclusionmode=0):
+    #Variables
+    #inim :                       input image
+    #windowsize :                 radius of exclusion range
+    #submask (optional) :         mask of potential peak regions
+    #prominence (optional) :      normalized (set to 0 to ignore), required prominence of peak
+    #normalmode (optional) :      normalization mode (0=full range, 1=percentile & crop)
+    #exclusionmode (optional) :   0=requires value to be peak value within radius, 1=not required to be peak
+
+    if len(insubmask.flatten())==0 or not np.all(np.shape(inim)==np.shape(insubmask)):
+        submask = np.ones_like(inim, dtype='bool')
+    else:
+        submask = np.copy(insubmask)
+
+    #outputs
+    outxyval = []                                 #peak x,y,value array
+    maxposn = np.empty([0],dtype='int')           #index position of maxima
+    pkprominence = np.empty([0],dtype='int')      #prominence of peak
+
+    #Normalize
+    if normalmode==0:   #full range
+        inim = inim-np.min(inim.flatten())
+        inim = inim/np.max(inim.flatten())
+    elif normalmode==1: #percentile
+        inim = inim-np.percentile(inim.flatten(),1)
+        inim = inim/np.percentile(inim.flatten(),99)
+
+    #Local variables
+    xx, yy = np.meshgrid(np.arange(np.shape(inim)[1]), np.arange(np.shape(inim)[0]))
+    xx = xx.flatten()                   #x position
+    yy = yy.flatten()                   #y position
+    val = inim.flatten()                #image value
+    vind = np.arange(len(val))          #indices
+    testflag = submask.flatten()        #flag whether to bother testing a point (are masked out when within exclusion radius of larger value)
+    sortind = np.flip(np.argsort(val))  #pre-sort by descending value 
+
+    #boundary mask
+    bmx,bmy = np.meshgrid(np.arange(-np.floor(windowsize/2),np.floor(windowsize/2)+1), np.arange(-np.floor(windowsize/2),np.floor(windowsize/2)+1))
+    b_mask = (bmx**2+bmy**2)**.5>np.floor(windowsize/2)
+    b_mask[np.floor(windowsize/2).astype('int'), np.floor(windowsize/2).astype('int')] = True
+
+    #Loop
+    n=0
+    for ii in np.arange(len(val)): 
+        if testflag[sortind[ii]]==True:
+        #get next viable maxima in sorted array
+        currpos = sortind[ii]
+        
+        #get window
+        xmin = np.maximum(xx[currpos]-np.floor(windowsize/2),0).astype('int')
+        xmax = np.minimum(xx[currpos]+np.floor(windowsize/2),np.shape(inim)[1]).astype('int')
+        xrng = np.arange(xmin,xmax,1,dtype='int')
+        ymin = np.maximum(yy[currpos]-np.floor(windowsize/2),0).astype('int')
+        ymax = np.minimum(yy[currpos]+np.floor(windowsize/2),np.shape(inim)[0]).astype('int')
+        yrng = np.arange(ymin,ymax,1,dtype='int')
+        #print(str(ii)+': x('+str(xmin)+':'+str(xmax)+') y('+str(ymin)+':'+str(ymax)+')')
+        frameim = inim[ymin:ymax,xmin:xmax]
+
+        #matching exclusion radius mask
+        xmin_bmsk = (np.floor(windowsize/2)-xx[currpos]+xmin).astype('int')
+        xmax_bmsk = (np.floor(windowsize/2)+xmax-xx[currpos]).astype('int')
+        ymin_bmsk = (np.floor(windowsize/2)-yy[currpos]+ymin).astype('int')
+        ymax_bmsk = (np.floor(windowsize/2)+ymax-yy[currpos]).astype('int')
+        #print(str(ii)+': x('+str(xmin_bmsk)+':'+str(xmax_bmsk)+') y('+str(ymin_bmsk)+':'+str(ymax_bmsk)+')')
+
+        mskind = np.where(b_mask[ymin_bmsk:ymax_bmsk,xmin_bmsk:xmax_bmsk].flatten()==False)
+
+        #prominence test
+        pk_p = val[currpos]-np.min(inim[ymin:ymax,xmin:xmax].flatten()[mskind])
+        if  pk_p < prominence:
+            #print(str(ii)+': prominence failure ('+str(pk_p))
+            continue
+
+        #mask out exlusion radius
+        submask[ymin:ymax,xmin:xmax]=np.logical_and(submask[ymin:ymax,xmin:xmax],b_mask[ymin_bmsk:ymax_bmsk,xmin_bmsk:xmax_bmsk])
+        lx,ly = np.meshgrid(xrng,yrng)
+        lxy = np.vstack([ly.flatten(),lx.flatten()])
+        sind = np.ravel_multi_index(lxy, np.shape(inim))
+        testflag[sind]=False
+
+        #local peak
+        if exclusionmode==0:
+            if np.any(frameim.flatten()[mskind]>=val[currpos]):    #check if max within radius
+            #print(str(ii)+': not regional max:')
+            continue
+        maxposn = np.append(maxposn,currpos)
+        pkprominence = np.append(pkprominence, pk_p)
+        n+=1
+    
+    outxyval = np.array([xx[maxposn],yy[maxposn],val[maxposn]])
+    return outxyval, maxposn, pkprominence, b_mask
