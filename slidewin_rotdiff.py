0dbef5573fbe815f29fdc603df36df1ba103e7e8
ctnelson <ctnelson@umich.edu>
2023-04-18 10:19:09 -0400

    Update slidewin_rotdiff.py

--- slidewin_rotdiff.py
+++ slidewin_rotdiff.py
@@ -388,14 +388,14 @@ def slidewin_rotdiff(inim, iang, irad, mode = 0, trygpu=True):
     #try:
     threadsperblock = 32
     blockspergrid = (iang.size + (threadsperblock - 1)) // threadsperblock
-    angarray_rotdiff_core_gpu[blockspergrid, threadsperblock](inim, iang, irad, mode, rotdif)
+    slidewin_rotdiff_core_gpu[blockspergrid, threadsperblock](inim, iang, irad, mode, rotdif)
     #except:
     #print('GPU Execution failed, fall back to cpu')
     #xx,yy = np.meshgrid(np.arange(0,inim_sz[0]+brdr*2,dtype=np.float32),np.arange(0,inim_sz[1]+brdr*2,dtype=np.float32))
-    #angarray_rotdiff_core_cpu(inim, iang, xx, yy, irad, mode, rotdif)
+    #slidewin_rotdiff_core_cpu(inim, iang, xx, yy, irad, mode, rotdif)
   else:
     xx,yy = np.meshgrid(np.arange(0,inim_sz[0]+brdr*2,dtype=np.float32),np.arange(0,inim_sz[1]+brdr*2,dtype=np.float32))
-    angarray_rotdiff_core_cpu(inim, iang, xx, yy, irad, mode, rotdif)
+    slidewin_rotdiff_core_cpu(inim, iang, xx, yy, irad, mode, rotdif)
 
   #outputs
   rotdif = rotdif[brdr:-brdr,brdr:-brdr]
