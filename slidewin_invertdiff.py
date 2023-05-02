3b9c619f65853551e79219895b03bc7594567e33
ctnelson <ctnelson@umich.edu>
2023-05-02 07:54:17 -0400

    Update slidewin_invertdiff.py

--- slidewin_invertdiff.py
+++ slidewin_invertdiff.py
@@ -210,7 +210,8 @@ def swinvrt_ccorr_GPU(inim, winrng, result_counts, result_mean, result_var, resu
             elif inim.ndim==2:
               result_var[ii,jj,0] += (inim[yy+y0,xx+x0]-result_mean[ii,jj,0])**2
               result_var[ii,jj,1] += (inim[y1-yy-1,x1-xx-1]-result_mean[ii,jj,1])**2
-    result_var[ii,jj,:] = result_var[ii,jj,:]/step/lp_3
+    result_var[ii,jj,0] = result_var[ii,jj,0]/step/lp_3
+    result_var[ii,jj,1] = result_var[ii,jj,1]/step/lp_3
     
     #The normalized cross correlation    
     for xx in range(x1-x0):
