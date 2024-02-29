c6c81d4eb943163f89ae08d785cf0e4978f041f6
ctnelson <ctnelson@umich.edu>
2024-02-29 14:15:14 -0500

    Update EELS_ZLP_Align.py

--- EELStools/EELS_ZLP_Align.py
+++ EELStools/EELS_ZLP_Align.py
@@ -2,7 +2,7 @@ from scipy import interpolate
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
-from ctntools.peakfitting import refinePeaks2D
+from ctntools.PeakFinding.peakfitting import refinePeaks2D
 from ctntools.BaseSupportFunctions.FWHM import FWHM
 
 #Aligns the EELS zero loss peak of an n-dim EELS dataset.  
