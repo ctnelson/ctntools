402af03d93b97e424b4458c8e2b691641db0796f
ctnelson <ctnelson@umich.edu>
2024-05-16 23:53:41 -0400

    Create pointInPolygon.py

---
+++ Geometry/pointInPolygon.py
@@ -0,0 +1,52 @@
+#Checks if test points lie in polygon
+import numpy as np
+from scipy.spatial import Delaunay
+
+#pointInTriangle  :  check if test points are within triangle
+#pointInPoly      :  check if test points are within polygon
+
+####################################################### Test if Points in Triangle #######################################
+#Checks if test points are within triangle
+#code snippet from Cédric Dufour https://stackoverflow.com/questions/2049582/how-to-determine-if-a-point-is-in-a-2d-triangle#:~:text=The%20point%20p%20is%20inside,coordinates%20of%20the%20point%20p%20.
+def pointInTriangle(tp, tri):
+    ### Inputs
+    #tp     :   [n,2] test points
+    #tri    :   [3,2] triangle vertices
+    ### Outputs
+    #output :   [n,]  whether test point lies within triangle
+  
+    if np.ndim(tp)==1:
+        tp=tp[np.newaxis,:]
+    assert(tp.shape[1]==2)
+
+    dX = tp[:,0]-tri[2,0]
+    dY = tp[:,1]-tri[2,1]
+    dX21 = tri[2,0]-tri[1,0]
+    dY12 = tri[1,1]-tri[2,1]
+    D = dY12*(tri[0,0]-tri[2,0]) + dX21*(tri[0,1]-tri[2,1])
+    s = dY12*dX + dX21*dY
+    t = (tri[2,1]-tri[0,1])*dX + (tri[0,0]-tri[2,0])*dY
+    if D<0:
+        output = ((s<=0) & (t<=0) & (s+t>=D))
+    else:
+        output = ((s>=0) & (t>=0) & (s+t<=D))
+
+    return output
+
+####################################################### Test if Points in Polygon #######################################
+#Checks if test points are within polygon
+#Polygon is defined by Delaunay triangulation
+def pointInPoly(tp,poly):
+    ### Inputs
+    #tp     :   [n,2] test points
+    #poly   :   [v,2] polygon vertices
+    ### Outputs
+    #output :   [n,]  whether test point lies within polygon
+    #sets   :   triangle sets from Delaunay
+  
+    output = np.zeros((tp.shape[0],))
+    tri = Delaunay(poly)
+    sets = tri.simplices
+    for i in range(sets.shape[0]):
+        output += pointInTriangle(tp,poly[sets[i,:],:])
+    return output, sets
