aac0fb929e08b462b822f93c72d8ce62f78c9c38
ctnelson <ctnelson@umich.edu>
2024-06-11 15:24:18 -0400

    Update simpleFuns.py

--- BaseSupportFunctions/simpleFuns.py
+++ BaseSupportFunctions/simpleFuns.py
@@ -13,9 +13,18 @@ def distWrapped(d1,d2,per=2*np.pi):
     #per   :    wrap period (defaults to radian circle 2*pi)
     ### Outputs ###
     #deld  :    distance(s)
-  
+
+    #Setup
     d1 = np.array(d1)
     d2 = np.array(d2)
-    assert d1.size==d2.size
+    if d1.size != d2.size:
+        if d2.size==1:
+            d2 = np.ones_like(d1)*d2
+        elif d1.size==1:
+            d1 = np.ones_like(d2)*d1
+        else:
+            raise ValueError('input sizes {:d},{:d} not compatible'.format(d1.size,d2.size))
+    #Function
     deld = (( (d2-d1) + per/2) % per - per/2)
+    #Return
     return deld
